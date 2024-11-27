66c2f0f5895ec458d58bdec711252bc435a9a434
Matt Pilott <matt@pilott.me>
2024-11-27 20:05:30 +0000

    whoops

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -7,8 +7,8 @@ const { rest: { repos: { getContent, listReleases }, issues: { getMilestone } } 
 })
 
 const githubs = [
-	{ owner: 'sveltejs', repo: 'svelte', path: 'packages/svelte/CHANGELOG.md', hideH1: true, milestone_number: 9 },
-	{ owner: 'sveltejs', repo: 'kit', path: 'packages/kit/CHANGELOG.md', hideH1: true, milestone_number: 8 },
+	{ owner: 'sveltejs', repo: 'svelte', path: 'packages/svelte/CHANGELOG.md', hideH1: true, milestone_number: 8 },
+	{ owner: 'sveltejs', repo: 'kit', path: 'packages/kit/CHANGELOG.md', hideH1: true, milestone_number: 7 },
 	{ owner: 'vitejs', repo: 'vite', path: 'packages/vite/CHANGELOG.md', hideH1: true, milestone_number: 22 },
 	{
 		rss: 'https://www.figma.com/release-notes/feed/atom.xml',
